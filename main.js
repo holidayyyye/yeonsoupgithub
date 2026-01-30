0aa331c9575ef0e3c92be4c55c7a8db50c8cc745
hash4766 <hash4766@gmail.com>
2026-01-30 13:39:36 +0000

    Initial commit

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-// Add JS here
+const lottoNumbersContainer = document.querySelector('.lotto-numbers');
+const generateButton = document.querySelector('#generate');
+
+generateButton.addEventListener('click', () => {
+    lottoNumbersContainer.innerHTML = '';
+    const numbers = new Set();
+    while (numbers.size < 6) {
+        numbers.add(Math.floor(Math.random() * 45) + 1);
+    }
+
+    const sortedNumbers = Array.from(numbers).sort((a, b) => a - b);
+
+    sortedNumbers.forEach((number, index) => {
+        setTimeout(() => {
+            const numberDiv = document.createElement('div');
+            numberDiv.classList.add('number');
+            numberDiv.textContent = number;
+            lottoNumbersContainer.appendChild(numberDiv);
+        }, index * 300);
+    });
+});
